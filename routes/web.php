df1e5bf34f474124e5afdf04f0ad91cd4b081062
nasim114 <nasimuddin1140@gmail.com>
2025-02-22 20:58:39 +0600

    remove home page

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,6 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PurchaseController;
 use App\Http\Controllers\SupplierController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
